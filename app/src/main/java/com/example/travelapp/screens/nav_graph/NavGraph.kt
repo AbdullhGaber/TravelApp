bc51ae95d709e9403a3080e580ddc6ed21968c2f
AbdullhGaber <abdullh760@yahoo.com>
2024-12-18 00:10:26 +0200

    + Adding NavGraph and Routes

---
+++ app/src/main/java/com/example/travelapp/screens/nav_graph/NavGraph.kt
@@ -0,0 +1,37 @@
+package com.example.travelapp.screens.nav_graph
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.example.travelapp.screens.register.RegisterScreen
+
+@Composable
+fun NavGraph(
+    startDestination : String
+){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = startDestination){
+        navigation(
+            route = Route.AuthNavigation.route,
+            startDestination = Route.RegisterScreen.route
+        ){
+            composable(
+                route = Route.RegisterScreen.route
+            ){
+               RegisterScreen(
+                   viewModel = hiltViewModel(),
+                   navigateToHome = {
+//                       navController.navigate(Route.UpComingScreen.route) {
+//                           popUpTo(Route.RegisterScreen.route) { inclusive = true } // Clears the back stack
+//                       }
+                   }
+               )
+            }
+        }
+    }
+}
+
