160a19254c50b353f1fa429bd623dbcb4729cd31
AbdullhGaber <abdullh760@yahoo.com>
2025-01-04 17:21:07 +0200

    - Remove trip class constants [one direction trip - round direction trip]

--- domain/src/main/java/com/example/domain/entity/TripEntity.kt
+++ domain/src/main/java/com/example/domain/entity/TripEntity.kt
@@ -16,9 +16,6 @@ data class TripEntity(
     companion object {
         const val TRIP_COLLECTION = "trips"
 
-        const val ONE_DIRECTION_TRIP = "One Direction"
-        const val ROUND_TRIP = "Round Trip"
-
         const val UPCOMING = "Upcoming"
         const val DONE = "Done"
         const val CANCELLED = "Cancelled"
