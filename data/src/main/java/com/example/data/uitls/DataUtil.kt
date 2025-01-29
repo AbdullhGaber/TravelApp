de683e74d8436361f2786acc2017660967bacb55
AbdullhGaber <abdullh760@yahoo.com>
2025-01-29 19:14:01 +0200

    + Inject context as global variable in data layer

--- data/src/main/java/com/example/data/uitls/DataUtil.kt
+++ data/src/main/java/com/example/data/uitls/DataUtil.kt
@@ -1,6 +1,13 @@
 package com.example.data.uitls
 
+import android.content.Context
 import com.example.domain.entity.TripUserEntity
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+@Inject
+@ApplicationContext
+lateinit var mContext : Context
 
 object DataUtil {
     var tripUser : TripUserEntity? = null
