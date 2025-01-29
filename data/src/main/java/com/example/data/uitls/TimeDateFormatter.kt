0de69c64050ef97378126d8945404dd8a03048fb
AbdullhGaber <abdullh760@yahoo.com>
2025-01-29 18:14:02 +0200

    + Implementing formatTimeDate function to parse date and time strings to date class

--- data/src/main/java/com/example/data/uitls/TimeDateFormatter.kt
+++ data/src/main/java/com/example/data/uitls/TimeDateFormatter.kt
@@ -5,6 +5,6 @@ import java.util.Date
 import java.util.Locale
 
 fun formatTimeDate(time : String , date : String) : Date?{
-    val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+    val formatter = SimpleDateFormat("yyyy-M-d hh:mm a", Locale.getDefault())
     return formatter.parse("$date $time")
 }
