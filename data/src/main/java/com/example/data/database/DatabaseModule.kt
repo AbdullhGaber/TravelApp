51083eccd7391e8399352ba4f55ac077a02085ed
AbdullhGaber <abdullh760@yahoo.com>
2025-02-17 18:22:42 +0200

    ~ Updating room database version and provide the converter class and trip dao

--- data/src/main/java/com/example/data/database/DatabaseModule.kt
+++ data/src/main/java/com/example/data/database/DatabaseModule.kt
@@ -21,7 +21,8 @@ class DatabaseModule {
             context,
             TravelRoomDatabase::class.java,
             "travel_db"
-        ).build()
+        ).fallbackToDestructiveMigration()
+        .build()
     }
 
     @Provides
@@ -29,5 +30,12 @@ class DatabaseModule {
         travelDB : TravelRoomDatabase
     ): UserDao{
         return travelDB.userDao
+    }
+
+    @Provides
+    fun provideTripDao(
+        travelDB : TravelRoomDatabase
+    ): TripDao{
+        return travelDB.tripDao
     }
 }
