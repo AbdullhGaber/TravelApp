0c33e2149367ae7633e99ad6cc6575e603179b81
AbdullhGaber <abdullh760@yahoo.com>
2025-02-17 18:20:03 +0200

    + make mapper for trip model and entity

--- data/src/main/java/com/example/data/mapper/EntityModelMappers.kt
+++ data/src/main/java/com/example/data/mapper/EntityModelMappers.kt
@@ -2,9 +2,11 @@ package com.example.data.mapper
 
 import com.example.data.model.ImageDataModel
 import com.example.data.model.ImageResponseModel
+import com.example.data.model.TripModel
 import com.example.data.model.TripUserModel
 import com.example.domain.entity.ImageDataEntity
 import com.example.domain.entity.ImageResponseEntity
+import com.example.domain.entity.TripEntity
 import com.example.domain.entity.TripUserEntity
 
 fun ImageResponseEntity.toModel() : ImageResponseModel{
@@ -37,4 +39,40 @@ fun TripUserEntity.toModel() : TripUserModel{
 
 fun TripUserModel.toEntity() : TripUserEntity{
     return TripUserEntity(uid, email, name, phoneNumber, imageURL, imagePath)
+}
+
+fun TripEntity.toModel() : TripModel{
+    return TripModel(
+       id ?:"",
+       uid,
+       notes,
+       status,
+       name,
+       startDestination,
+       endDestination,
+       date,
+       time,
+       returnDate,
+       returnTime,
+       type,
+       hasTimeCome
+    )
+}
+
+fun TripModel.toEntity() : TripEntity{
+    return TripEntity(
+       id,
+       uid,
+       notes,
+       status,
+       name,
+       startDestination,
+       endDestination,
+       date,
+       time,
+       returnDate,
+       returnTime,
+       type,
+       hasTimeCome
+    )
 }
