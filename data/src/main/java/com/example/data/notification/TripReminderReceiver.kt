345e7f8535887916eaaaaa0a6b22b953d4753175
AbdullhGaber <abdullh760@yahoo.com>
2025-02-24 19:13:30 +0200

    ~ depending on TripUseCases rather than TripDao, so to not access data source directly

--- data/src/main/java/com/example/data/notification/TripReminderReceiver.kt
+++ data/src/main/java/com/example/data/notification/TripReminderReceiver.kt
@@ -3,17 +3,16 @@ package com.example.data.notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.example.data.database.TripDao
 import com.example.data.uitls.Constants.TRIP_END_DESTINATION_KEY
 import com.example.data.uitls.Constants.TRIP_ID_KEY
 import com.example.data.uitls.Constants.TRIP_NAME_KEY
 import com.example.data.uitls.Constants.TRIP_START_DESTINATION_KEY
 import com.example.domain.repositories.trip.NotificationHandler
+import com.example.domain.use_cases.trip.TripUseCases
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,8 +20,9 @@ import javax.inject.Inject
 class TripReminderReceiver: BroadcastReceiver(){
     @Inject
     lateinit var mTripNotificationHandler: NotificationHandler
+
     @Inject
-    lateinit var mTripDao: TripDao
+    lateinit var mTripUseCases: TripUseCases
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO + Job())
 
@@ -31,9 +31,9 @@ class TripReminderReceiver: BroadcastReceiver(){
         val tripName = intent?.getStringExtra(TRIP_NAME_KEY) ?: "Your Trip"
         val tripStartDes = intent?.getStringExtra(TRIP_START_DESTINATION_KEY) ?: "Your Start Destination"
         val tripEndDes = intent?.getStringExtra(TRIP_END_DESTINATION_KEY) ?: "Your End Destination"
-        mTripNotificationHandler.showTripReminderNotification(tripName,tripStartDes,tripEndDes)
+        mTripNotificationHandler.startService(tripId,tripName,tripStartDes,tripEndDes)
         coroutineScope.launch {
-            mTripDao.setHasTimeComeToTrue(tripId)
+            mTripUseCases.updateTripHasTimeComeUseCase(tripId,1)
         }
     }
 }
