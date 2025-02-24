bfcc5b6a00c08faa937b2bac6f53de29444ba14b
AbdullhGaber <abdullh760@yahoo.com>
2025-02-24 18:15:51 +0200

    + add UpdateTripHasTimeComeUseCase use case class

---
+++ domain/src/main/java/com/example/domain/use_cases/trip/UpdateTripHasTimeComeUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.domain.use_cases.trip
+
+import androidx.annotation.IntRange
+import com.example.domain.repositories.trip.TripRepository
+import javax.inject.Inject
+
+class UpdateTripHasTimeComeUseCase @Inject constructor(
+    private val mTripRepository: TripRepository
+) {
+    suspend operator fun invoke(id : String, @IntRange(0,1) value : Int){
+        mTripRepository.updateTripHasTimeCome(id,value)
+    }
+}
