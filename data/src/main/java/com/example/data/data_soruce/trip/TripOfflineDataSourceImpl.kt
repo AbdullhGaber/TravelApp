90bd6543962d53ea75a7cd8d83144469cc61a039
AbdullhGaber <abdullh760@yahoo.com>
2025-02-24 19:07:00 +0200

    + implement updateHasTimeComeInTrip() TripOfflineDataSourceImpl

--- data/src/main/java/com/example/data/data_soruce/trip/TripOfflineDataSourceImpl.kt
+++ data/src/main/java/com/example/data/data_soruce/trip/TripOfflineDataSourceImpl.kt
@@ -1,14 +1,13 @@
 package com.example.data.data_soruce.trip
 
+import androidx.annotation.IntRange
 import com.example.data.database.TripDao
 import com.example.data.mapper.toEntity
 import com.example.data.mapper.toModel
 import com.example.domain.entity.TripEntity
 import com.example.domain.repositories.trip.TripOfflineDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 class TripOfflineDataSourceImpl @Inject constructor(
@@ -24,5 +23,9 @@ class TripOfflineDataSourceImpl @Inject constructor(
 
     override fun getScheduledTrips(): Flow<List<TripEntity>> {
         return mTripDao.getScheduledTrips().map{ it.map { it.toEntity() } }
+    }
+
+    override suspend fun updateTripHasTimeCome(id: String,@IntRange(0,1) value: Int) {
+        mTripDao.updateHasTimeCome(id, value)
     }
 }
