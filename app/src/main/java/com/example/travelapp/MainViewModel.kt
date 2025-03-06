2e8a9bcf50e0f717f531f3184e7b5d97a90b78eb
AbdullhGaber <abdullh760@yahoo.com>
2025-03-06 16:59:00 +0200

    - Remove scheduled trips code from mainViewModel

--- app/src/main/java/com/example/travelapp/MainViewModel.kt
+++ app/src/main/java/com/example/travelapp/MainViewModel.kt
@@ -1,22 +1,15 @@
 package com.example.travelapp
 
 import android.util.Log
-import androidx.annotation.IntRange
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.data.uitls.DataUtil
-import com.example.data.uitls.Resource
-import com.example.domain.entity.TripEntity
 import com.example.domain.manager.LocalUserManager
-import com.example.domain.use_cases.trip.TripUseCases
 import com.example.domain.use_cases.user.UserUseCases
 import com.example.travelapp.screens.nav_graph.Route
-import com.example.travelapp.screens.upcoming.UpcomingEvents
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -24,7 +17,6 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val mLocalUserManager: LocalUserManager,
     private val mUserUseCases: UserUseCases,
-    private val mTripUseCases: TripUseCases,
 ) : ViewModel() {
     var startDestination = mutableStateOf(Route.AuthNavigation.route)
         private set
@@ -32,54 +24,9 @@ class MainViewModel @Inject constructor(
     var shouldSplashScreenOn = mutableStateOf(true)
         private set
 
-    private val _scheduledTripsStateFlow = MutableStateFlow<Resource<List<TripEntity>?>>(Resource.Unspecified())
-    val scheduledTripsStateFlow = _scheduledTripsStateFlow.asStateFlow()
-
     init{
         setStartDestination()
-        getScheduledTrips()
     }
-
-    fun onEvent(event : UpcomingEvents){
-        when(event){
-            is UpcomingEvents.OnTripReminderDialogCancelClick -> {
-                dismissTripReminderDialog()
-                clearScheduledTripFlowState()
-                updateHasTimeComeInTrip(event.id, event.value)
-            }
-
-            is UpcomingEvents.OnTripCardDeleteClick -> Unit
-
-            is UpcomingEvents.OnUndoDeleteClick -> Unit
-        }
-    }
-    private fun updateHasTimeComeInTrip(id : String, @IntRange(0,1) value : Int){
-        viewModelScope.launch {
-            try{
-                mTripUseCases.updateTripHasTimeComeUseCase(id,value)
-            }catch(e : Exception){
-                Log.e("MainViewModel Error", e.message.toString())
-            }
-        }
-    }
-    private fun getScheduledTrips(){
-        viewModelScope.launch {
-            _scheduledTripsStateFlow.emit(Resource.Loading())
-        }
-
-        viewModelScope.launch {
-            try{
-                mTripUseCases.getScheduledTrips().collect{trips ->
-                    _scheduledTripsStateFlow.emit(Resource.Success(trips))
-                }
-            }catch (e : Exception){
-                viewModelScope.launch {
-                    _scheduledTripsStateFlow.emit(Resource.Failure(e.message))
-                }
-            }
-        }
-    }
-
     private fun setStartDestination(){
         viewModelScope.launch {
             mLocalUserManager.getUserUID().collect{ uid ->
@@ -107,34 +54,5 @@ class MainViewModel @Inject constructor(
                 Log.e("DataStore Error","No user found")
             }
         )
-    }
-
-    private val shouldShowTripReminderDialog = mutableStateOf(false)
-
-    fun setTripDataForNotification(
-        id : String,
-        name : String,
-        startDes : String,
-        endDes : String
-    ){
-        val trip = TripEntity(id = id, name = name, startDestination = startDes, endDestination = endDes)
-        viewModelScope.launch {
-           _scheduledTripsStateFlow.emit(Resource.Success(listOf(trip)))
-        }
-    }
-
-    fun getShouldShowTripReminderDialog() = shouldShowTripReminderDialog.value
-
-    private fun clearScheduledTripFlowState(){
-        viewModelScope.launch {
-            _scheduledTripsStateFlow.emit(Resource.Unspecified())
-        }
-    }
-    fun showTripReminderDialog() {
-        shouldShowTripReminderDialog.value = true
-    }
-
-    fun dismissTripReminderDialog() {
-        shouldShowTripReminderDialog.value = false
     }
 }
