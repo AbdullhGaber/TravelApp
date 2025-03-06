192e6c24f1bf9c701de0d5c6c77758e320b76845
AbdullhGaber <abdullh760@yahoo.com>
2025-03-06 17:27:18 +0200

    ~ Changing updateHasTimeCome() to updateHasFirstTripTimeCome() in trip dao + Adding updateHasSecondTripTimeCome()

--- data/src/main/java/com/example/data/database/TripDao.kt
+++ data/src/main/java/com/example/data/database/TripDao.kt
@@ -2,23 +2,24 @@ package com.example.data.database
 
 import androidx.annotation.IntRange
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.data.model.TripModel
-import com.example.domain.entity.TripEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TripDao {
-    @Query("UPDATE trips SET hasTimeCome = :value WHERE id = :id")
-    suspend fun updateHasTimeCome(id : String, @IntRange(0,1) value : Int)
+    @Query("UPDATE trips SET hasFirstTripTimeCome = :value WHERE id = :id")
+    suspend fun updateHasFirstTripTimeCome(id : String, @IntRange(0,1) value : Int)
+
+    @Query("UPDATE trips SET hasSecondTripTimeCome = :value WHERE id = :id")
+    suspend fun updateHasSecondTripTimeCome(id : String, @IntRange(0,1) value : Int)
 
     @Query("SELECT * FROM trips")
     fun getAllTrips() : Flow<List<TripModel>>
 
-    @Query("SELECT * FROM trips WHERE hasTimeCome = 1")
+    @Query("SELECT * FROM trips WHERE hasFirstTripTimeCome = 1 OR hasSecondTripTimeCome = 1")
     fun getScheduledTrips() : Flow<List<TripModel>>
 
     @Insert
